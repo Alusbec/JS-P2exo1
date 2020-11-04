5ce5a42f4aa73de73c43866ebef241cde18a3506
Alusbec <adrienlsbc@gmail.com>
2020-11-04 09:48:05 +0100

    exo1p2

---
+++ asset/JS/style.js
@@ -0,0 +1,6 @@
+function setNewImage(){
+    document.getElementById("img1").src = "asset/img/Soleil.png"}
+
+function setOldImage(){
+    document.getElementById("img1").src = "asset/img/lune.png"
+}
